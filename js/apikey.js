f54545aeddb35a39fe78b7a8a400c927e0c64893
EbukaNweje <nwejeebuka@gmail.com>
2022-06-20 16:51:44 +0100

    Push ApiKey

---
+++ js/apikey.js
@@ -0,0 +1,15 @@
+// const API_KEY = 'AIzaSyB7hTgKuE5-iUWFJ8mKJihLoAl4O92pd0M'
+// console.log(API_KEY)
+// export default API_KEY;
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB7hTgKuE5-iUWFJ8mKJihLoAl4O92pd0M',
+  authDomain: 'eflexajegunlecode.firebaseapp.com',
+  databaseURL: 'https://eflexajegunlecode-default-rtdb.firebaseio.com',
+  projectId: 'eflexajegunlecode',
+  storageBucket: 'eflexajegunlecode.appspot.com',
+  messagingSenderId: '696065757903',
+  appId: '1:696065757903:web:c055f4e152695c9f9bddba',
+};
+
+export default firebaseConfig;
